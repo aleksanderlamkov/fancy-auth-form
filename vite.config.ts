bc8140a8382cf421adc1afb373d2e8194ffac780
Aleksander Lamkov <aleksanderlamkov@gmail.com>
2022-08-27 20:51:38 +0500

    Update gitignore and vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { createSvgIconsPlugin } from "vite-plugin-svg-icons"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/fancy-auth-form/",
   plugins: [
     react(),
     createSvgIconsPlugin({
