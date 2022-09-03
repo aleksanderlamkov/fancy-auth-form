bc0676e26935ad692d57172a43a1d4a93abb5e08
Aleksander Lamkov <aleksanderlamkov@gmail.com>
2022-09-03 18:14:16 +0500

    fix(validation)

--- src/hooks/useUserForm.ts
+++ src/hooks/useUserForm.ts
@@ -19,10 +19,10 @@ export const useUserForm = (hasPasswordConfirm: boolean = false) => {
           .required("Required field")
           .min(8, "Cannot be less than 8 characters")
           .max(32, "Cannot be more than 32 characters")
-          .matches(/^[A-Za-z]+$/, "Only english letters")
           .matches(/[a-z]+/, "Must contain at least one lowercase character")
           .matches(/[A-Z]+/, "Must contain at least one uppercase character")
-          .matches(/\d+/, "Must contain at least one number"),
+          .matches(/\d+/, "Must contain at least one number")
+          .matches(/^[a-zA-Z0-9!@#\$%\^\&*\)\(+=._-]+$/g, "Only english letters, numbers and default special characters"),
         passwordConfirm: Yup.string()
           .required("Required field")
           .oneOf([ Yup.ref("password") ], "Password must be the same"),
